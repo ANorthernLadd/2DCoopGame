1a57f9f4b1d437b1645e7b1fd00aa16de9fc6600
ANorthernLadd <68791617+ANorthernLadd@users.noreply.github.com>
2020-09-18 01:51:08 -0400

    Update StartMenu.gd

--- Projects/StartMenu.gd
+++ Projects/StartMenu.gd
@@ -2,7 +2,7 @@
 extends Control
 
 func _on_Start_pressed():
-	get_tree().change_scene("res://Worlds/World.tscn")
+	get_tree().change_scene("res://Worlds/World_V2.tscn")
 
 
 func _on_Quit_pressed():
